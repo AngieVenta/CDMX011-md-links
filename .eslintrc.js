ddcee3aaa0de3807e2b50375e9811f6d2561a217
AngieVenta <86637560+AngieVenta@users.noreply.github.com>
2021-10-20 01:39:41 -0500

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,34 +18,7 @@ module.exports = {
     'no-console': ['off'],
     'linebreak-style': ['error', 'windows'],
     'object-shorthand': ['off'],
-    'max-len': ['off'], // off or warn
+    'max-len': ['off'],
     'consistent-return': ['off'],
     'no-else-return': ['off'],
-  },
-=======
-    "env": {
-        "commonjs": true,
-        "es2021": true,
-        "node": true
-    },
-    "extends": [
-        "airbnb-base"
-    ],
-    "parserOptions": {
-        "ecmaVersion": 12,
-        "sourceType": "module"
-    },
-    "rules": {
-        "semi": ["error", "always"],
-        "quotes": ["error", "single"],
-        "no-console": ["off"],
-        "linebreak-style": ["error", "windows"],
-        "object-shorthand": ["off"],
-        "max-len": ["off"],
-        "arrow-body-style": ["off"],
-        "no-return-await": ["off"],
-        "consistent-return": ["off"],
-        "no-else-return": ["off"]
-    }
->>>>>>> a1bfeefd27ade047531c68ad3f239457f9b16487
-};
+  }
